e28d013cf0f23748f8d99a4a9333b69d46aaf3da
rabbanos <59020772+rabbanos@users.noreply.github.com>
2022-11-15 09:55:42 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/img/logo.png" height="100">
+  <h1>My Collection</h1>
+  <ul>
+    <!-- <li v-for="(pokemon, index) in pokemonList" :key="`poke-${index}`">
+      <a :href="pokemon.url">{{ pokemon.name }}</a>
+    </li> -->
+    <li v-for="(detail, index) in pokemonDetail" :key="index">
+      <img :src="detail.sprites.other.home.front_default">
+      <p style="color:black">name: {{ detail.name }}</p>
+      <p style="color:black">Height: {{ detail.height }}</p>
+    </li>
+  </ul>
 </template>
-
+<!-- https://pokeapi.co/api/v2/pokemon-species/1/ -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    pokemonList: [],
+    pokemonDetail: []
+  }),
+  async mounted() {
+    const pokeData = await fetch('https://pokeapi.co/api/v2/pokemon').then(
+      response => response.json()
+    )
+    // console.log(pokeData.results)
+
+    for (let index = 0; index < pokeData.results.length; index++) {
+      const element = pokeData.results[index];
+      // console.log(element)
+      const data = await fetch(element.url).then(
+        response => response.json())
+      // ini
+      console.log(data)
+      this.pokemonDetail.push(data)
+    }
+    this.pokemonList = pokeData.results
   }
 }
 </script>
@@ -20,7 +45,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #eee;
   margin-top: 60px;
 }
 </style>
